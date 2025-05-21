4b658230d921f6aebfe9c70f4661e09d366db9c3
karuppurajav20 <karuppurajav20@gmail.com>
2025-05-21 10:56:32 +0530

    first commit

--- event management/backend/server/server.js
+++ event management/backend/server/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const eventRouter = require('./route/eventroute'); 
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 
 app.use(cors());
